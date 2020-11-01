429e53605c0d8a2078cabca97005d81d94f929ad
Revanshi <54432157+Revanshi@users.noreply.github.com>
2020-11-01 10:22:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.use('/users' , require('./routes/users'));
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT , console.log(`Server started on ${PORT}`));
